fff32f6a80aefaaf6409f3c1b3296189fe55d940
Mark Voltaire <markjvoltaire@gmail.com>
2024-01-21 11:12:11 -0600

    added navigation

--- App.jsx
+++ App.jsx
@@ -1,14 +1,46 @@
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { supabase } from "./supabase/supabase";
+import { UserProvider } from "./context/UserContext";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { StyleSheet, Text, View } from "react-native";
+import NoAuth from "./auth/NoAuth";
+import Auth from "./auth/Auth";
 
 export default function App() {
+  const [auth, setAuth] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleAuthStateChange = (_event, session) => {
+      if (_event === "SIGNED_IN") {
+        setAuth(session);
+      } else if (_event === "SIGNED_OUT") {
+        setAuth(null);
+      }
+    };
+
+    const session = supabase.auth.session();
+    console.log("session", session);
+    setAuth(session);
+
+    supabase.auth.onAuthStateChange(handleAuthStateChange);
+
+    return () => {
+      supabase.auth.offAuthStateChange(handleAuthStateChange);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to tizly</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <UserProvider>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          {auth === null ? <NoAuth /> : <Auth />}
+        </View>
+      </UserProvider>
+    </NavigationContainer>
   );
 }
 
@@ -16,7 +48,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
--- auth/NoAuth.jsx
+++ auth/NoAuth.jsx
@@ -1,11 +1,46 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Welcome from "../screens/Welcome";
+import { UserProvider, useUser } from "../context/UserContext";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
+import ResetPassword from "../screens/ResetPassword";
 
-export default function NoAuth() {
+export default function NoAuthStack({ auth }) {
+  const { user, setUser } = useUser();
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>NoAuth</Text>
-    </View>
+    <UserProvider>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false, gestureEnabled: false }}
+        />
+
+        <Stack.Screen
+          name="ResetPassword"
+          component={ResetPassword}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </UserProvider>
   );
 }
 
