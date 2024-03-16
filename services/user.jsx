481d99fae43ab794f7ec05eed98f14cb3d2af3fc
Mark Voltaire <markjvoltaire@gmail.com>
2024-03-16 16:37:26 -0500

    First Tizly Service Static Build

--- services/user.jsx
+++ services/user.jsx
@@ -124,6 +124,7 @@ export async function getRandomUser() {
   const response = await supabase
     .from("profiles")
     .select("*")
+    .eq("type", "business")
     .neq(
       "bannerImage",
       "https://ivxipgaauikqwyguqagw.supabase.co/storage/v1/object/public/profile-images/noProfileAVI.jpeg"
